3a9b36d75e1c7989d7f9b634247be12f3cfb6350
dua-tiga-exe <ask.frhmn23@gmail.com>
2022-11-10 15:57:47 +0700

    ok

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Whynu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    # config.hosts << "3b4f-103-106-219-60.ngrok.io"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
